d9bddfc6f62e3c2777e34ba32c152a94ad7ad6c7
ktjybl <64632527+ktjybl@users.noreply.github.com>
2021-09-23 11:54:15 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,16 @@
+﻿const burger=document.querySelector('.humburger-menu');
+const menuElem=document.querySelector('.menu');
+
+const toggleMenu=()=>{
+   burger.classList.toggle('humburger-menu-active');
+   menuElem.classList.toggle('menu-active');
+};
+
+burger.addEventListener('click',toggleMenu);
+
+menuElem.addEventListener('click', (event) => {
+	const target = event.target;
+	if (target.classList.contains('menu-list__link')) {
+		toggleMenu()
+	}
+})
---
+++ js/modal.js
@@ -0,0 +1,16 @@
+﻿const modalBtns=document.querySelectorAll('.more');
+const modal=document.querySelector('.modal');
+
+modalBtns.forEach(item=>{
+    item.addEventListener('click', ()=>{
+        modal.classList.remove('hidden');
+    })
+});
+
+modal.addEventListener('click', event=>{
+     event.preventDefault();
+     const target=event.target;
+     if(target.classList.contains('overlay')||target.classList.contains('modal__close')){
+        modal.classList.add('hidden');
+     }
+});
